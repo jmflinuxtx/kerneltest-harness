d87bda4be32bf1bd39d459008a746de6ef467bc1
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-04-24 15:20:06 +0200

    pep8 fixes

--- frontend/default_config.py
+++ frontend/default_config.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-SECRET_KEY='<change me before using me in prod>'
+SECRET_KEY = '<change me before using me in prod>'
 
-DB_URL='sqlite:////var/tmp/kernel-test_dev.sqlite'
+DB_URL = 'sqlite:////var/tmp/kernel-test_dev.sqlite'
 
-LOG_DIR='logs'
+LOG_DIR = 'logs'
 
-API_KEY='This is a secret only the cli knows about'
+API_KEY = 'This is a secret only the cli knows about'
